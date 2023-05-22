10e10ea1bcdfed2f3fe056a0ffd0cfd8312f2cb5
degull <skytmdgus591@naver.com>
2023-05-22 15:33:55 +0900

    동기 & 비동기통신 pr

---
+++ class/pages/rest-get/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import axios from 'axios';
+import {useState} from 'react';
+
+export default function RestGetPage(){
+
+   const [title, setTitle] = useState("")
+
+   /* 비동기 */
+   function onClickAsync(){
+      const result = axios.get("https://koreanjson.com/posts/1")
+      console.log(result)
+   }
+
+   /* 동기 */
+   // async function onClickSync(){    => 원래방식
+      const onClickSync = async () => {
+      const result = await axios.get("https://koreanjson.com/posts/1")
+      console.log(result)
+      console.log(result.data)
+      console.log(result.data.title)
+      setTitle(result.data.title)
+   }
+
+   return (
+      <>
+         <button onClick={onClickAsync}>REST-API 비동기 요청하기</button>
+         <button onClick={onClickSync}>REST-API 동기 요청하기</button>
+         <div>{title}</div>
+      </>
+   );
+};
+
